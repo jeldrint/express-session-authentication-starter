1305f5ac6bdbe7d92d11be69068f0e1b04befe1f
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2024-01-31 16:01:46 +0800

    adding dotenv

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
-var passport = require('passport');
-var crypto = require('crypto');
-var routes = require('./routes');
+const passport = require('passport');
+const crypto = require('crypto');
+const routes = require('./routes');
 const connection = require('./config/database');
 
 // Package documentation - https://www.npmjs.com/package/connect-mongo
 const MongoStore = require('connect-mongo')(session);
 
-// Need to require the entire Passport config module so app.js knows about it
-require('./config/passport');
 
 /**
  * -------------- GENERAL SETUP ----------------
@@ -20,24 +18,38 @@ require('./config/passport');
 require('dotenv').config();
 
 // Create the Express application
-var app = express();
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
 
 /**
  * -------------- SESSION SETUP ----------------
  */
 
-// TODO
+const sessionStore = new MongoStore( {mongooseConnection: connection, collection: 'sessions'})
+
+app.use(session({
+    secret: 'cats',
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 //equals 1 day
+    }
+}))
+
+
 
 /**
  * -------------- PASSPORT AUTHENTICATION ----------------
  */
+// Need to require the entire Passport config module so app.js knows about it
+require('./config/passport')
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
 
 /**
--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,45 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const connection = require('./database');
+const { validPassword } = require('../lib/passwordUtils');
 const User = connection.models.User;
 
-// TODO: passport.use();
+const customFields = {
+    usernameField: 'uname',
+    passwordField: 'pw'
+}
+
+const verifyCallback = async (username, password, done) => {
+    try {
+        const user = await User.findOne({ username: username })
+        if (!user) {
+            return done(null, false, {message: 'Incorrect username'})
+        }
+
+        const isValid = validPassword(password, user.hash, user.salt)
+
+        if(isValid){
+            return done(null,user)
+        }else{
+            return done(null,false)
+        }
+    }catch(err){
+        return done(err)
+    }
+    
+}
+
+passport.use(new LocalStrategy(customFields, verifyCallback));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+});
+
+passport.deserializeUser(async (userId, done) =>{
+    try {
+        const user = await User.findById(userId);
+        done(null,user)
+    }catch(err){
+        done(err);
+    }
+})
